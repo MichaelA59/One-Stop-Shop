189603dc2a392d895faa7653ec7e15fd5bd0f741
savini419 <stevensavini@gmail.com>
2017-06-15 13:39:24 -0400

    user log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615164757) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "userauths", force: :cascade do |t|
-    t.string   "first_name",         null: false
-    t.string   "last_name",          null: false
-    t.string   "username",           null: false
-    t.string   "email",              null: false
-    t.string   "phone_number"
-    t.string   "encrypted_password", null: false
-    t.string   "salt"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-  end
 
 end
