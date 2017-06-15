bf5db2f0d311a36c4c0bbe1a57feb2b3c3368592
Michael Adamski <mvadamski@gmail.com>
2017-06-15 13:15:05 -0400

    added .env file and stored twitter keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['95zHIVCZeQ87VN4i6HCKXnMcf'], ENV['WrltxcfyogOGsaDO4rj1ELUVD7ODGIBeuhflKL5eZZjT7fEfJo']
+  provider :twitter, '95zHIVCZeQ87VN4i6HCKXnMcf', 'WrltxcfyogOGsaDO4rj1ELUVD7ODGIBeuhflKL5eZZjT7fEfJo'
 end
